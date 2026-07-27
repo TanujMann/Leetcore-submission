309e70d8fe7389cedbfa41beeb841e97aff0659b
Tanuj Mann <149461756+TanujMann@users.noreply.github.com>
2026-07-27 15:33:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (77.43%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds = minutesToTest / minutesToDie;
+        int pigs = 0;
+        long long states = 1;
+
+        while (states < buckets) {
+            pigs++;
+            states *= (rounds + 1);
+        }
+
+        return pigs;
+    }
+};
